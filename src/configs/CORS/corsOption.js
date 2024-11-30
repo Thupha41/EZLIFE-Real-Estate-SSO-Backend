1fa82139ff222b00ccae043cb4d3b751df069dfd
Thupha41 <phatngo.040103@gmail.com>
2024-11-30 14:53:22 +0700

    Fix cookie v32

--- src/configs/CORS/corsOption.js
+++ src/configs/CORS/corsOption.js
@@ -86,6 +86,7 @@ const corsOption = {
       callback(new Error("Not allowed by CORS"));
     }
   },
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: [
     "Content-Type",
